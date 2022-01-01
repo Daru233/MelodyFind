b01d39e612e08b8d1abd4a0248c211a71f5079d1
Michael Malto <michaeldm233@gmail.com>
2022-01-01 14:23:50 +0000

    created skeleton Flask app, added imports

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+import CONSTANTS
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+CLIENT_ID = CONSTANTS.Client_ID
+CLIENT_SECRET = CONSTANTS.Client_Secret
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
+
     return "Hello, World!"
 
 
