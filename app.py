a1e004958bc2ce55e15d58a807b8e2d053d61ce8
Michael Malto <michaeldm233@gmail.com>
2022-01-02 17:58:39 +0000

    set up auth, added sessions folder to gitignore

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, session, request, redirect
+from flask_session import Session
+import uuid
+import os
 import CONSTANTS
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
@@ -8,8 +11,82 @@ from flask_cors import CORS
 CLIENT_ID = CONSTANTS.Client_ID
 CLIENT_SECRET = CONSTANTS.Client_Secret
 REDIRECT_URL = CONSTANTS.Redirect_URL
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(64)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = './.flask_session/'
+Session(app)
 CORS(app)
+
+caches_folder = './.spotify_caches/'
+if not os.path.exists(caches_folder):
+    os.makedirs(caches_folder)
+
+
+def session_cache_path():
+    return caches_folder + session.get('uuid')
+
+
+@app.route('/callback', methods=['GET'])
+def callback():
+
+    if not session.get('uuid'):
+        # Step 1. Visitor is unknown, give random ID
+        session['uuid'] = str(uuid.uuid4())
+
+    cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
+    auth_manager = spotipy.oauth2.SpotifyOAuth(client_id=CLIENT_ID,
+                                               client_secret=CLIENT_SECRET,
+                                               redirect_uri=REDIRECT_URL,
+                                               scope='user-read-currently-playing playlist-modify-private',
+                                               cache_handler=cache_handler,
+                                               show_dialog=True)
+
+    if request.args.get("code"):
+        # Step 3. Being redirected from Spotify auth page
+        auth_manager.get_access_token(request.args.get("code"))
+        # return redirect('/callback')
+
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        # Step 2. Display sign in link when no token
+        auth_url = auth_manager.get_authorize_url()
+        return f'<h2><a href="{auth_url}">Sign in</a></h2>'
+
+    # Step 4. Signed in, display data
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    # return f'<h2>Hi {spotify.me()["display_name"]}, ' \
+    #        f'<small><a href="/sign_out">[sign out]<a/></small></h2>' \
+    #        f'<a href="/playlists">my playlists</a> | ' \
+    #        f'<a href="/currently_playing">currently playing</a> | ' \
+    #        f'<a href="/current_user">me</a>' \
+    return jsonify("Signed in successfully!", 200)
+
+
+@app.route('/sign_out')
+def sign_out():
+    try:
+        # Remove the CACHE file (.cache-test) so that a new user can authorize.
+        os.remove(session_cache_path())
+        session.clear()
+    except OSError as e:
+        print ("Error: %s - %s." % (e.filename, e.strerror))
+    return redirect('/callback')
+
+@app.route('/current_user')
+def current_user():
+    cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler,
+                                               client_id=CLIENT_ID,
+                                               client_secret=CLIENT_SECRET,
+                                               redirect_uri=REDIRECT_URL)
+
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        return redirect('/callback')
+
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    return spotify.current_user()
+
 
 @app.route("/mf/v1/song", methods=["GET"])
 def getARandomSong():
@@ -38,7 +115,7 @@ def getARandomSong():
     for item in playlist_items_result['items']:
         tracks_in_playlist.append(item)
 
-    track_to_return = tracks_in_playlist[randint(0, playlist_items_result['total']-1)]
+    track_to_return = tracks_in_playlist[randint(0, playlist_items_result['total'] - 1)]
     response.append(playlist_items_result)
 
     return jsonify(track_to_return, 200)
